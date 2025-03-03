8d722ad78d18597df7f772086139fa31759662a0
SyedIrfanali03 <greenstaskmay@gmail.com>
2025-03-03 16:58:49 +0530

    some code added

---
+++ src/org/sample/Project.java
@@ -0,0 +1,31 @@
+package org.sample; //package
+
+public class Project { // class
+
+	private void task1() {// method
+
+		System.out.println("My name : Demo code "); // business logic
+		System.out.println("My address : no 12 smartkills Tech");
+
+	}
+
+	private void task2() {
+
+		System.out.println("Demo started");
+		System.out.println("Demo End");
+
+	}
+
+	public static void main(String[] args) {// main method
+
+		// Classname objrefname=new Classname();
+		
+		Project p = new Project();   //object 
+
+		p.task1();//to call method
+		p.task2();
+		
+		
+	}
+
+}
